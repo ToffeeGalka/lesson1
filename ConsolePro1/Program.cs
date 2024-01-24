bf13adb866f909c03037c965249cc6fcb4ea9ec1
Toffee <toffee@tut.by>
2024-01-24 17:11:28 +0300

    Add project files.

---
+++ ConsolePro1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using libPro1;
+
+Console.WriteLine(ClassPro1.ClassPro1Print());
---
+++ libPro1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace libPro1
+{
+    public static class ClassPro1
+    {
+        public static string ClassPro1Print()
+        {
+            return "HELLO, TOFFEE!";
+        }
+    }
+}
+
